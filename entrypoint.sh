97723bc75ec5dece211b5a43ca51922b73b6d00f
f0rkz <f0rkz@f0rkznet.net>
2022-10-23 18:08:20 -0400

    Update env for k8s support soon to come

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,11 @@ steamcmd.sh +force_install_dir /data/barotrauma \
 mkdir -p /root/.steam/sdk64
 ln -s /opt/steamcmd/linux64/steamclient.so /root/.steam/sdk64/steamclient.so
 
-envtmpl /opt/configuration/serversettings.xml.tmpl > /data/barotrauma/serversettings.xml
-
-cp /opt/configuration/clientpermissions.xml /data/barotrauma/Data/clientpermissions.xml
+# We are generating this kube
+if [ "$SETTINGS_ENVTPL" == "true" ]
+    envtmpl /opt/configuration/serversettings.xml.tmpl > /data/barotrauma/serversettings.xml
+    cp /opt/configuration/clientpermissions.xml /data/barotrauma/Data/clientpermissions.xml
+fi
 
 cd /data/barotrauma
 ./DedicatedServer
